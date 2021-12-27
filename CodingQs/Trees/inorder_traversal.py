80a878c8fc4b3a2b3552b55bd023cc3b6b33e488
IvanRado <ivanradovicc@gmail.com>
2021-12-27 01:50:21 -0500

    Allegedly something changed

--- CodingQs/Trees/inorder_traversal.py
+++ CodingQs/Trees/inorder_traversal.py
@@ -47,3 +47,28 @@ class Solution2:
         
         helper(root, ans)
         return ans
+
+
+# Iterative solution
+class Solution3:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        nodes_val,track=[],[]
+        
+        while root!=None:
+            
+            if root.left!=None:
+                track.append(root)  # since Left node should come first, we keep root for next turn
+                root=root.left
+            elif root.right!=None:   # when left is None but right node exists
+                nodes_val.append(root.val)  # we push the root value and traverse the right sub tree
+                root=root.right
+            else:
+                nodes_val.append(root.val)   # when we have reached a leaf node, we push the value and pop from stack
+                if len(track)>0:
+                    root=track.pop()
+                    if root!=None:
+                        root.left=None  #  eliminating the left sub tree, since it has already been traversed
+                else:
+                    root=None  # when track is empty
+                    
+        return nodes_val
