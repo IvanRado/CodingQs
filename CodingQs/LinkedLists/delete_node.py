cedf5f6aa3da13018401207cb2213d4845417201
IvanRado <ivanradovicc@gmail.com>
2022-03-11 17:05:35 -0500

    Modified delete LL node question

---
+++ CodingQs/LinkedLists/delete_node.py
@@ -0,0 +1,12 @@
+# Given  a pointer to a linked list, delete it in place
+# Time Complexity: O(1), Space complexity: O(1)
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        next_node = node.next
+        node.val = next_node.val
+        node.next = next_node.next
+        
